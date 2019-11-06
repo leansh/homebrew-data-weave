d347ba086c3186f39ccd5fbae094565ff4e8ad84
Leandro Shokida <leandroshokida@gmail.com>
2019-11-06 13:48:32 -0300

    foo

--- formula/dw.rb
+++ formula/dw.rb
@@ -1,11 +1,6 @@
-class DW < Formula
-  name "dw"
-  desc "DataWeave CLI"
-  homepage 'https://github.com/mulesoft-labs/data-weave-native'
-  url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20191105/native-cli-2.3.0-20191105-native-distro-osx.zip"
+bottle do
+  # desc "DataWeave CLI"
+  # homepage "https://github.com/mulesoft-labs/data-weave-native"
+  root_url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20191105/native-cli-2.3.0-20191105-native-distro-osx.zip"
   sha256 "2b9cf05b09ce1dfb0ae755a320ee203cc419c3db11f5b5ec66e967d083eb515f"
-
-  def install
-    prefix.install "dw"
-  end
 end
