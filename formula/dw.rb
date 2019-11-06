6a3af5fe8a88f640ef2ef5378d690d8f36b8ee50
Leandro Shokida <leandroshokida@gmail.com>
2019-11-06 15:33:44 -0300

    changing version

--- formula/dw.rb
+++ formula/dw.rb
@@ -3,6 +3,7 @@ class Dw < Formula
   homepage "https://github.com/mulesoft-labs/data-weave-native"
   url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20191106/native-cli-2.3.0-20191106-native-distro-osx.zip"
   sha256 "2b9cf05b09ce1dfb0ae755a320ee203cc419c3db11f5b5ec66e967d083eb515f"
+  version "2.3.0-20191106"
 
   def install
     prefix.install "bin"
